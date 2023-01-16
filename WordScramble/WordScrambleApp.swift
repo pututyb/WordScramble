38347337de6f518a0e8615553503339d79c2bcfb
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-01-16 13:49:29 +0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Putut Yusri Bahtiar on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
